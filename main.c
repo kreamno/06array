2714b868ecfedc21e4206f16cdfa7e5d8765f0a2
yujeong <kreamno@gmail.com>
2018-11-01 15:39:17 +0900

    array 01 (수정)

--- main.c
+++ main.c
@@ -9,10 +9,10 @@ void printMatrix(int A[][COLS])		//앞에 []에는 ROWS 써도 되고 안 써도 됨
 	
 	for (i=0; i<ROWS; i++) {
 		for (j=0; j<COLS; j++) {
-			printf("%d", A[i][j]);
+			printf("%d ", A[i][j]);
 		}
 		
-		printf("\n");
+		printf(" \n");
 	}
 	
 	return;		//void를 하면 return 뒤에 수 붙이면 안 됨. 
@@ -35,14 +35,14 @@ int addMatrix (int A[][COLS], int B[][COLS], int C[][COLS])
 int main(int argc, char *argv[]) {
 	
 	int A[ROWS][COLS]={			//더하기 대상 행렬 
-		{2,3,0},
-		{8,9,1},
-		{7,0,5}
+		{2, 3, 0},
+		{8, 9, 1},
+		{7, 0, 5}
 		};
 	int B[ROWS][COLS]={			//더하기 대상 행렬 
-		{1,0,0},
-		{0,1,0},
-		{0,0,1}
+		{1, 0, 0},
+		{0, 1, 0},
+		{0, 0, 1}
 		};
 	int C[ROWS][COLS];			//덧셈 결과 행렬 
 	
